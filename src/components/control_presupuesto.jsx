d92a788c90dc700668283fb9564305326dbb0f1d
lODIN007l <wilsonlluilema007@gmail.com>
2022-07-01 20:28:14 -0500

    presupuesto y validacion del mismo

---
+++ src/components/control_presupuesto.jsx
@@ -0,0 +1,22 @@
+function ControlPresupuesto({ presupuesto }) {
+  const formatearCantidad = (cantidad) => {
+    return cantidad.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  };
+
+  return (
+    <div className="contenedor-presupuesto  contenedor sombra dos-columnas">
+      <div>
+        <p>GRAFICA AQUI</p>
+      </div>
+      <div className="contenido-presupuesto">
+        <p>
+          <span>Presupuesto:</span>${formatearCantidad(presupuesto)}
+        </p>
+      </div>
+    </div>
+  );
+}
+export default ControlPresupuesto;
